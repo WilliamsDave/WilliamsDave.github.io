83ee5fffc710c2182a17bdc478f0a4dddd34d67d
David Williams <120070689+WilliamsDave@users.noreply.github.com.>
2024-09-13 13:16:15 -0400

    added swiper to about page

--- scripts.js
+++ scripts.js
@@ -1,9 +1,32 @@
+//swiper script from swiperjs.com
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+        slidesPerView: 'auto',
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2500,
+          },
+    
+
+    });
+});
+
 //sticky header
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
     header.classList.toggle('sticky', window.scrollY > 0);
 });
-    
+
 
 //form submit from internet
 //script no longer works need to update
@@ -12,26 +35,26 @@ const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg");
 form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent, thank you!";
-        setTimeout(function(){
-            msg.innerHTML = "";
-        },5000)
-        form.reset();
-    })
-    .catch(error => console.error('Error!', error.message))
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent, thank you!";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000)
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
 })
 
 //small screen tabs
 let tabLinks = document.getElementsByClassName("tab-links");
 let tabContents = document.getElementsByClassName("tab-content");
 
-function opentab(tabname){
-    for(tabLink of tabLinks){
+function opentab(tabname) {
+    for (tabLink of tabLinks) {
         tabLink.classList.remove("active-link");
     }
-    for(tabContent of tabContents){
+    for (tabContent of tabContents) {
         tabContent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
@@ -43,14 +66,14 @@ let navLinks = document.querySelectorAll('.navLink');
 let openBtn = document.querySelector('.openBtn');
 let closeBtn = document.querySelector('.closeBtn');
 
-openBtn.addEventListener("click",()=>{
+openBtn.addEventListener("click", () => {
     sidebar.classList.add('opened');
 });
-closeBtn.addEventListener("click",()=>{
+closeBtn.addEventListener("click", () => {
     sidebar.classList.remove('opened');
 });
-navLinks.forEach(link=>{
-    link.addEventListener("click",()=>{
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
         sidebar.classList.remove('opened');
     });
 });
